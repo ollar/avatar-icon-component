cc88d7dcea98d0eca1c3a13d6b056225ca1b1414
Oleg Larkin <olegollar@gmail.com>
2019-03-13 23:50:21 +0300

    tests added

--- tests/integration/components/avatar-icon-test.js
+++ tests/integration/components/avatar-icon-test.js
@@ -1,26 +1,65 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | avatar-icon', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  hooks.beforeEach(function() {
+    const userData = {
+      username: 'test'
+    };
+    this.set('userData', userData);
 
+    const imageModel = {
+      url: '//joeschmoe.io/api/v1/random'
+    };
+    this.set('imageModel', imageModel);
+  });
+
+  test('it renders', async function(assert) {
     await render(hbs`{{avatar-icon}}`);
 
+    assert.equal(this.element.textContent.trim(), 'A');
+  });
+
+  test('test user data', async function(assert) {
+    await render(hbs`{{avatar-icon data=userData}}`);
+    assert.equal(this.element.textContent.trim(), 'T');
+
+    this.set('userData.username', 'john doe');
+    await render(hbs`{{avatar-icon data=userData}}`);
+    assert.equal(this.element.textContent.trim(), 'JD');
+  });
+
+  test('empty user data', async function(assert) {
+    this.set('userData', null);
+    await render(hbs`{{avatar-icon data=userData}}`);
+    assert.equal(this.element.textContent.trim(), 'A');
+
+    this.set('userData', undefined);
+    await render(hbs`{{avatar-icon data=userData}}`);
+    assert.equal(this.element.textContent.trim(), 'A');
+  });
+
+  test('empty username', async function(assert) {
+    this.set('userData.username', 0);
+    await render(hbs`{{avatar-icon data=userData}}`);
     assert.equal(this.element.textContent.trim(), '');
 
-    // Template block usage:
-    await render(hbs`
-      {{#avatar-icon}}
-        template block text
-      {{/avatar-icon}}
-    `);
+    this.set('userData.username', '');
+    await render(hbs`{{avatar-icon data=userData}}`);
+    assert.equal(this.element.textContent.trim(), '');
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    this.set('userData.username', null);
+    await render(hbs`{{avatar-icon data=userData}}`);
+    assert.equal(this.element.textContent.trim(), '');
+  });
+
+  test('test userImage', async function(assert) {
+    await render(hbs`{{avatar-icon image=imageModel}}`);
+    assert.ok(find('img'));
+    assert.equal(this.element.querySelector('img').getAttribute('src'), this.imageModel.url);
   });
 });
